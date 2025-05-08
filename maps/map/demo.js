ae07b89b30171255c8a0424213127e833cd8eca9
Malloc72P <scra1028@gmail.com>
2025-05-08 16:00:46 +0900

    Chore: 첫번째 피들 데모 추가

---
+++ maps/map/demo.js
@@ -0,0 +1,25 @@
+const config = {
+  title: '세계지도',
+  map: [{
+    url: `../../assets/map/wooritech/v1/maps/geojson/world-low.geo.json`
+  }],
+  body: {
+    projection: 'mercator'
+  },
+  series: [{
+    useMapData: true,
+    style: {
+      fill: 'var(--area-color-1)',
+      stroke: '#fff',
+      strokeWidth: 0.7
+    },
+    hoverColor: '#83A8DC',
+    tooltipText: '<t style="font-weight: bold;">${name}(${rm-id})</t>'
+  }],
+  exporting: {
+    visible: true
+  }
+};
+async function init() {
+  await RealMap.createChartAsync(document, 'realmap', config, true);
+}
